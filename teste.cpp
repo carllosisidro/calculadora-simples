7bcde0bbb098dd72687eb6ae3f0276554573f105
carlos <carlosdelima_@hotmail.com>
2024-04-01 23:10:08 -0300

    calculadora

--- teste.cpp
+++ teste.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <string>
+using  namespace std;
+int main()
+{
+     int a1, a2, total;
+ 
+ char op;
+  cout<< "valor";
+  cin>>a1; 
 
-int main(){
-    cout<<"hello word";
-    cout<<"ola";
+  cout<<"qual operacao vc quer: +, -, *, / ";
+  cin>>op;
+ 
+  cout<<"digite outro numero ";
+  cin>>a2;
+ 
+if (op == '+')
+{
+   total = a1 + a2;
+}
+else if (op == '-')
+{
+    total = a1 - a2;
+}
+if (op == '*')
+{
+   total = a1 * a2;
+}
+if (op == '/')
+{
+   total = a1 / a2;
+}
+ 
+  cout<<"o total e "<<total;
+  
+
+
 }
